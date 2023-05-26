724d9d135966377e132ab9e9370446ecffcb0fcd
dunzolo <rossidavide108@gmail.com>
2023-05-26 14:35:59 +0200

    first commit

---
+++ src/main/java/org/lessons/java/CrudPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPizzeriaApplication.class, args);
+	}
+
+}
