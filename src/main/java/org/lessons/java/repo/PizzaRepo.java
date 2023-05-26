cb0ce4f55ae7be4682152b5ce898b9c511d6c1dc
dunzolo <rossidavide108@gmail.com>
2023-05-26 17:47:10 +0200

    exercise done

--- src/main/java/org/lessons/java/repo/PizzaRepo.java
+++ src/main/java/org/lessons/java/repo/PizzaRepo.java
@@ -2,7 +2,9 @@ package org.lessons.java.repo;
 
 import org.lessons.java.pojo.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PizzaRepo extends JpaRepository<Pizza, Integer>{
 
 }
